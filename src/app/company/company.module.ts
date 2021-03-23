35fe0e637927b6cbacc7aa8c68bd644925c5b6ad
Mihir Joshi <mihir67mj@gmail.com>
2021-03-23 15:02:15 +0530

    Added Components

--- src/app/company/company.module.ts
+++ src/app/company/company.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
+import { ListComponent } from './list/list.component';
+import { ViewComponent } from './view/view.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AddComponent, EditComponent, ListComponent, ViewComponent],
   imports: [
     CommonModule
   ]
